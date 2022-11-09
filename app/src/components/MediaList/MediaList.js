9f3cb8d4a54cc5aaa801ac6a0eeaf7b9b9749481
gm2870 <alifarzanegan70@gmail.com>
2022-11-09 16:29:49 +0330

    updated medialist

--- app/src/components/MediaList/MediaList.js
+++ app/src/components/MediaList/MediaList.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMediaItems } from '../../store/redux/media/media-actions';
 import MediaItem from '../MediaItem/MediaItem';
@@ -12,14 +12,15 @@ const MediaList = ({ sliderItems }) => {
       dispatch(getMediaItems());
     }
   }, []);
+
   if (!items) return <div>data not found</div>;
   const content = sliderItems.visible.map((itemIndex) => (
     <div key={items[itemIndex].id} className={classes.media__item}>
       <MediaItem item={items[itemIndex]} />
     </div>
   ));
-  if (!items) return <div>Data not found</div>;
   return <div className={classes.media}>{content}</div>;
 };
 
 export default MediaList;
+// https://blog.bitsrc.io/5-ways-to-avoid-react-component-re-renderings-90241e775b8c
