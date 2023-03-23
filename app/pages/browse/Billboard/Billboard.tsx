368eaa184e6ca7e5a334d010282db53f9dab0a07
gm2870 <alifarzanegan70@gmail.com>
2023-03-23 23:11:12 +0430

    added info to billboard

--- app/pages/browse/Billboard/Billboard.tsx
+++ app/pages/browse/Billboard/Billboard.tsx
@@ -4,10 +4,12 @@ import { useRef } from 'react';
 import CircleButton from '../../../src/components/CircleButton/CircleButton';
 import VideoJS from '../../../src/components/VideoJS/VideoJS';
 import videojs from 'video.js';
-
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import classes from './Billboard.module.scss';
 import { Media } from '../../../src/store/redux/media/model';
 import { useAppSelector } from '../../../src/hooks';
+import CustomButton from '../../../src/components/CustomButton/CustomButton';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const Billboard = ({ item }: { item: Media }) => {
   const playerRef = useRef<videojs.Player>(null);
@@ -58,7 +60,18 @@ const Billboard = ({ item }: { item: Media }) => {
       playerRef.current?.play();
     }
   };
+  const fancyTextGenerator = (text: string): string => {
+    const words = text.split('-');
+    const withoutDash = words.join(' ');
+    console.log(withoutDash);
+    const breaksLength = words.length;
+    let numOfSpans = 1;
 
+    if (breaksLength > 1) {
+      numOfSpans = Math.ceil(breaksLength / 2);
+    }
+    return withoutDash;
+  };
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
   }, []);
@@ -84,6 +97,31 @@ const Billboard = ({ item }: { item: Media }) => {
             />
           </div>
         )}
+        <div className={classes.info}>
+          <div className={classes.info__fantcyTitle}>
+            {fancyTextGenerator(item.title)}
+          </div>
+          <div className={classes.info__description}>{item.overview}</div>
+          <div className={classes.info__actions}>
+            <CustomButton
+              variant="contained"
+              white={true}
+              size="medium"
+              className={classes.colorPrimary}
+            >
+              <PlayArrowIcon
+                className={`${classes.info__icon} ${classes['info__icon--black']}`}
+              />
+              <span className={classes.info__text}>Play</span>
+            </CustomButton>
+            <CustomButton variant="contained" className={classes.gray}>
+              <ErrorOutlineIcon
+                className={`${classes.info__icon} ${classes['info__icon--black']}`}
+              />
+              <span className={classes.info__text}>More Info</span>
+            </CustomButton>
+          </div>
+        </div>
         <div className={classes.actions}>
           {playing ? (
             <CircleButton onClick={toggleSoundHandler}>
--- app/src/components/CustomButton/CustomButton.tsx
+++ app/src/components/CustomButton/CustomButton.tsx
@@ -2,27 +2,31 @@ import { Button } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { PropsWithChildren, ComponentPropsWithoutRef } from 'react';
-
+import classes from './CustomButton.module.scss';
 type ButtonProps = ComponentPropsWithoutRef<typeof Button>;
 type CustomButtonProps = PropsWithChildren<
-  ButtonProps & { dynamicSize: boolean }
+  ButtonProps & { dynamicSize?: boolean; white?: boolean }
 >;
 
 const CustomButton = ({
   children,
   dynamicSize,
+  white,
   ...props
 }: CustomButtonProps) => {
   const theme = useTheme();
   const min600 = useMediaQuery(theme.breakpoints.up('sm'));
   const getSize = () => {
     if (!dynamicSize || min600) {
-      return 'medium';
+      return 'large';
     }
     return 'small';
   };
   return (
-    <Button {...props} size={getSize()}>
+    <Button
+      className={`${classes.button} ${white ? classes['button--white'] : ''}`}
+      {...props}
+    >
       {children}
     </Button>
   );
